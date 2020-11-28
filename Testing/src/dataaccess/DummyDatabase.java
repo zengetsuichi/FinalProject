038aa5e5642d4dd366a1bfee084387c0a68cb6c8
Malgorzata Drygala <49656312+gosiadrygala@users.noreply.github.com>
2020-11-28 18:51:02 +0100

    Dummy database

---
+++ Testing/src/dataaccess/DummyDatabase.java
@@ -0,0 +1,67 @@
+package dataaccess;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.sql.*;
+
+import org.apache.ibatis.jdbc.ScriptRunner;
+
+public class DummyDatabase
+{
+  private String url = "jdbc:postgresql://localhost:5432/postgres?currentSchema=pricechecker";
+  private String username = "postgres";
+  private String password = "database123";
+  private Connection con;
+
+  public DummyDatabase()
+  {
+    try
+    {
+      //Registering the Driver
+      DriverManager.registerDriver(new org.postgresql.Driver());
+      //Getting the connection
+      con = DriverManager.getConnection(url, username, password);
+    }
+    catch (SQLException throwables)
+    {
+      throwables.printStackTrace();
+    }
+
+  }
+  public void createDummyDatabase()
+  {
+    try
+    {
+      System.out.println("Connection established......");
+      //Initialize the script runner
+      ScriptRunner sr = new ScriptRunner(con);
+      //Creating a reader object
+      Reader reader = null;
+      reader = new BufferedReader(new FileReader("Testing/dummydatabase.sql"));
+      //Running the script
+      sr.runScript(reader);
+    }
+    catch (FileNotFoundException e)
+    {
+      e.printStackTrace();
+    }
+
+  }
+
+  public void dropDatabase()
+  {
+    try
+    {
+      PreparedStatement statement = con.prepareStatement("DROP schema pricecheckerdummy cascade");
+      statement.executeUpdate();
+    }
+    catch (SQLException throwables)
+    {
+      throwables.printStackTrace();
+    }
+  }
+
+
+}
